e93b89800069751a3bb015563ff0702231e58914
Kaio Piola <35163593+kaiopiola@users.noreply.github.com>
2023-05-22 18:15:20 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 $now = time(); // or your date as well
-$your_date = strtotime("2010-01-31");
+$your_date = strtotime("2023-05-12");
 $datediff = $now - $your_date;
 
 echo round($datediff / (60 * 60 * 24));
